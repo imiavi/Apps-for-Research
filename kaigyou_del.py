c5a16fd1968dfef5a881285326044852f63a3caf
imiavi <107638056+imiavi@users.noreply.github.com>
2023-04-19 17:54:35 +0900

    Update kaigyou_del.py

--- kaigyou_del.py
+++ kaigyou_del.py
@@ -1,7 +1,7 @@
 file_name = "kaigyou_del.txt"　#ファイル名
 
 with open(file_name, encoding="utf-8") as f:
-    line = f.read().replace('\n', ' ').replace('. ', '.\n')
+    line = f.read().replace('\n', ' ').replace('.', '.\n').replace('\n ', '\n')
     
 with open(file_name, mode="w", encoding="utf-8") as f:
     f.write(line)
